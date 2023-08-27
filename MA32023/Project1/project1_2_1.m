7170c8a571eda07d1c86a922209cbc46a1b51f9f
shx-haah <143275690+shx-haah@users.noreply.github.com>
2023-08-27 19:47:27 +0800

    Update project1_2_1.m

--- MA32023/Project1/project1_2_1.m
+++ MA32023/Project1/project1_2_1.m
@@ -1,11 +1,11 @@
-n = 0;
-N = 0 : 1 : 20;
-x = 10;
+n = 0 ;
+N = 0 : 1 : 20 ;
+x = 10 ;
 for i = 1 : 1 : 21
-    SN(i) = 0;
+    SN(i) = 0 ;
     for n = 0 : 1 : i
-        SN(i) = SN(i) + (x^n) / prod(1 : n);
+        SN(i) = SN(i) + (x^n) / prod(1 : n) ;
     end
-    Er(i) = abs((SN(i) - exp(x)) / exp(x));
+    Er(i) = abs((SN(i) - exp(x)) / exp(x)) ;
 end
-plot(N,Er);
+plot(N,Er) ;
